6eb081d0300302060d2d55169f3228f20ecc9ffb
Arif Widayana <arifwidayana@gmail.com>
2023-01-14 21:48:42 +0700

    fixed negative case when has no collection movies

--- app/src/main/java/com/arifwidayana/themoviedb/presentation/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/arifwidayana/themoviedb/presentation/ui/favorite/FavoriteFragment.kt
@@ -3,6 +3,7 @@ package com.arifwidayana.themoviedb.presentation.ui.favorite
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import androidx.core.view.isVisible
 import androidx.lifecycle.asLiveData
 import com.arifwidayana.themoviedb.common.base.BaseFragment
 import com.arifwidayana.themoviedb.common.wrapper.Resource
@@ -39,12 +40,27 @@ class FavoriteFragment : BaseFragment<FragmentFavoriteBinding, FavoriteViewModel
         }
     }
 
+    override fun showContent(isVisible: Boolean) {
+        super.showContent(isVisible)
+        binding.swipeRefreshLayout.isVisible = isVisible
+    }
+
+    override fun showEmptyContent(isVisible: Boolean) {
+        super.showEmptyContent(isVisible)
+        binding.clErrorView.isVisible = isVisible
+    }
+
     override fun observeData() {
         viewModel.getFavoriteResult.asLiveData().observe(viewLifecycleOwner) {
             if (it is Resource.Success) {
+                showContent(true)
+                showEmptyContent(false)
                 setStateAdapter(it.data)
                 Log.d("TAG", "observeData: ${it.data}")
             } else if (it is Resource.Error) {
+                showContent(false)
+                showEmptyContent(true)
+                setEmptyState(it.message)
                 Log.d("TAG", "observeData: ${it.exception} or ${it.message}")
             }
         }
@@ -61,6 +77,12 @@ class FavoriteFragment : BaseFragment<FragmentFavoriteBinding, FavoriteViewModel
             }
             adapter.submitList(data)
             rvFavorite.adapter = adapter
+        }
+    }
+
+    private fun setEmptyState(data: String?) {
+        binding.apply {
+            tvMessageError.text = data
         }
     }
 }
