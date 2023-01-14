b921b8b11010cda06faeb15525710e91b26f4e67
Arif Widayana <arifwidayana@gmail.com>
2023-01-14 21:29:34 +0700

    loading state paging adapter

---
+++ app/src/main/java/com/arifwidayana/themoviedb/presentation/adapter/home/LoadingStateAdapter.kt
@@ -0,0 +1,48 @@
+package com.arifwidayana.themoviedb.presentation.adapter.home
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.arifwidayana.themoviedb.databinding.ItemLoadingMoviesBinding
+
+class LoadingStateAdapter(
+    private val retry: () -> Unit
+) : LoadStateAdapter<LoadingStateAdapter.LoadingStateViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        loadState: LoadState
+    ): LoadingStateViewHolder {
+        val binding = ItemLoadingMoviesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return LoadingStateViewHolder(binding, retry)
+    }
+
+    override fun onBindViewHolder(holder: LoadingStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    class LoadingStateViewHolder(
+        private val binding: ItemLoadingMoviesBinding,
+        retry: () -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.retryButton.setOnClickListener { retry.invoke() }
+        }
+
+        @SuppressLint("SetTextI18n")
+        fun bind(loadState: LoadState) {
+            binding.apply {
+                if (loadState is LoadState.Error) {
+                    errorMsg.text = "Something went wrong or request timeout!"
+                }
+                shimmerPlaceholder.isVisible = loadState is LoadState.Loading
+                retryButton.isVisible = loadState is LoadState.Error
+                errorMsg.isVisible = loadState is LoadState.Error
+            }
+        }
+    }
+}
