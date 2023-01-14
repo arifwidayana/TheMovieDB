8765883dc3c6097eb4ebacf996f858c703449b29
Arif Widayana <arifwidayana@gmail.com>
2023-01-14 21:25:25 +0700

    add common utils

---
+++ app/src/main/java/com/arifwidayana/themoviedb/common/utils/Constant.kt
@@ -0,0 +1,22 @@
+package com.arifwidayana.themoviedb.common.utils
+
+object Constant {
+    //Database
+    const val DB_NAME = "movie.db"
+
+    //Key URL
+    const val API_NAME = "api_key"
+
+    //Path URL
+    const val ID_PATH = "movie_id"
+
+    //Query URL
+    const val PAGE = "page"
+
+    //Index
+    const val DEFAULT_INDEX_PAGE = 1
+    const val NETWORK_PAGE_SIZE = 25
+
+    //Playback
+    const val DEFAULT_PLAYBACK_VIDEO = 0F
+}
